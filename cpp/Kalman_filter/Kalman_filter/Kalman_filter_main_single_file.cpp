9ab0203051d273522b7abd18c01682f1106e4d5a
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2021-02-14 21:32:16 +0200

    code organized

--- cpp/Kalman_filter/Kalman_filter/Kalman_filter_main_single_file.cpp
+++ cpp/Kalman_filter/Kalman_filter/Kalman_filter_main_single_file.cpp
@@ -16,11 +16,40 @@ MatrixXd Q;
 
 vector <VectorXd> measurements;
 void filter(VectorXd &x, MatrixXd &P);
+void test_eigen_lib(void);
 
 int main()
 {
+
+
+
+	return 0;
+}
+
+void filter(VectorXd &x, MatrixXd &P)
+{
+
+
+}
+
+void test_eigen_lib()
+{
 	VectorXd my_vector(2);
 	my_vector << 1, 2;
-	cout << my_vector << endl;
-	return 0;
+	cout << "my vector \n" << my_vector << endl;
+
+	MatrixXd mymatrix(2, 2);
+	mymatrix << 1, 2, 3, 4;
+	mymatrix(1, 0) = 11;
+	mymatrix(1, 1) = 12;
+	cout << "mymatrix \n" << mymatrix << endl;
+
+	/** matrix transpose */
+	MatrixXd my_matrix_t = mymatrix.transpose();
+	cout << "Matrix transpose \n" << my_matrix_t << endl;
+
+	/** matrix inverse */
+	MatrixXd my_matrix_inv = mymatrix.inverse();
+	cout << "Matrix inverse \n" << my_matrix_inv << endl;
+
 }
