5cb3e39e278148e9fa0ed84f0d4660764cfed5be
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2021-03-04 15:25:19 +0200

    sigma point prediction

--- cpp/Kalman_filter/Kalman_filter/main.cpp
+++ cpp/Kalman_filter/Kalman_filter/main.cpp
@@ -1,48 +1,28 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <vector>
 #include "Eigen/Dense"
-#include "measurement_package.h"
-#include "tracking.h"
-#include "tools.h"
-#include "FusionEKF.h"
+#include "ukf.h"
 
 using Eigen::MatrixXd;
-using Eigen::VectorXd;
-using std::cout; 
-using std::endl;
-using std::ifstream;
-using std::istringstream;
-using std::string;
-using std::vector;
-
 
 int main() {
 
+	// Create a UKF instance
+	UKF ukf;
+
 	/**
-	 * Set Measurements
+	 * Programming assignment calls
 	 */
-	MeasurementPackage meas_package;
-	Tools tools;
-	FusionEKF fusionEKF;
+	MatrixXd Xsig = MatrixXd(5, 11);
+	ukf.GenerateSigmaPoints(&Xsig);
 
-	meas_package.sensor_type_ = MeasurementPackage::DVL;
-	meas_package.raw_measurements_ = VectorXd(2);
+	/**
+	 * Programming assignment calls
+	 */
+	MatrixXd Xsig_aug = MatrixXd(7, 15);
+	ukf.AugmentedSigmaPoints(&Xsig_aug);
 
-	meas_package.raw_measurements_ << 8.44818, 0.251553;
-	meas_package.timestamp_ = 1477010443449633;
-	fusionEKF.ProcessMeasurement(meas_package);
-
-	meas_package.raw_measurements_ << 8.44818, 0.251553;
-	meas_package.timestamp_ = 1477010443449633;
-	fusionEKF.ProcessMeasurement(meas_package);
-
-	meas_package.raw_measurements_ << 8.45582, 0.253997;
-	meas_package.timestamp_ = 1477010443549747;
-	fusionEKF.ProcessMeasurement(meas_package); 
-	
+	MatrixXd Xsig_pred = MatrixXd(15, 5);
+	ukf.SigmaPointPrediction(&Xsig_pred);
 
 	return 0;
 }
-
---
+++ cpp/Kalman_filter/Kalman_filter/main_ekf.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include "Eigen/Dense"
+#include "measurement_package.h"
+#include "tracking.h"
+#include "tools.h"
+#include "FusionEKF.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::cout; 
+using std::endl;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+
+int mainekf() {
+
+	/**
+	 * Set Measurements
+	 */
+	MeasurementPackage meas_package;
+	Tools tools;
+	FusionEKF fusionEKF;
+
+	meas_package.sensor_type_ = MeasurementPackage::DVL;
+	meas_package.raw_measurements_ = VectorXd(2);
+
+	meas_package.raw_measurements_ << 8.44818, 0.251553;
+	meas_package.timestamp_ = 1477010443449633;
+	fusionEKF.ProcessMeasurement(meas_package);
+
+	meas_package.raw_measurements_ << 8.44818, 0.251553;
+	meas_package.timestamp_ = 1477010443449633;
+	fusionEKF.ProcessMeasurement(meas_package);
+
+	meas_package.raw_measurements_ << 8.45582, 0.253997;
+	meas_package.timestamp_ = 1477010443549747;
+	fusionEKF.ProcessMeasurement(meas_package); 
+	
+
+	return 0;
+}
+
